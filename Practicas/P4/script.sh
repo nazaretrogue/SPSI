891c68e583c7d1b72393b31b01b4c1d775486b31
Nazaret <nazaretrogue@correo.ugr.es>
2019-11-21 17:10:32 +0100

    Añadido script p4

---
+++ Practicas/P4/script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Preparación de la infraestructura de directorios
+mkdir -p root/ca
+cd root/ca
+mkdir certs crl newcerts private
+chmod 700 private
+touch index.txt
+echo 1000 > serial
+
+# Modificación del archivo de configuración
+atom openssl.cnf
+pwd
+
+# Creamos la clave de root
+openssl genrsa -aes256 -out private/ca.key.pem 4096
+chmod 400 private/ca.key.pem
+
+# Creamos el certificado de root
+openssl req -config openssl.cnf -key private/ca.key.pem -new x509 -days 7300 -sha256 \
+        -extensions v3_ca -out certs/ca.cert.pem
+chmod 444 certs/ca.cert.pem
+
+# Verificamos el certificado
+openssl x509 -noout -text -in certs/ca.cert.pem
